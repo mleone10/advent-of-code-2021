679cdc101634ea8ee7db6e7da8643c35c6e708f1
Mario Leone <mleone10@gmail.com>
2021-12-01 09:43:36 -0500

    Day 1 solutions

---
+++ README.md
@@ -0,0 +1,18 @@
+# Advent of Code 2021
+> Solutions to 2021's daily challenges
+
+This year's solutions are written in TypeScript!  I'm still figuring this language out, but some requirements are probably:
+* Node
+* NPM
+
+# Build
+Solutions must be compiled from TypeScript to JavaScript before they can be run:
+```bash
+npx tsc
+```
+
+# Execute
+Solutions are designed to accept input from STDIN:
+```bash
+cat days/day1/day1.txt | node dist/day1.js
+```
---
+++ days/day1/day1.ts
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  cflfDelay: Infinity
+});
+
+let depths: Array<number> = []
+rl.on("line", (line: string) => {
+  depths.push(parseInt(line));
+});
+
+rl.on("close", () => {
+  console.log(calcDepthIncreases(depths));
+  console.log(calcRelativeDepthIncreases(depths));
+});
+
+function calcDepthIncreases(depths: Array<number>): number {
+  let increases: number = 0
+  depths.slice(0, -1).forEach((d, i) => {
+    if (d < depths[i + 1]) {
+      increases += 1;
+    }
+  })
+  return increases;
+}
+
+function calcRelativeDepthIncreases(depths: Array<number>): number {
+  let increases: number = 0;
+  depths.slice(0, -2).forEach((d, i) => {
+    if (d + depths[i + 1] + depths[i + 2] < depths[i + 1] + depths[i + 2] + depths[i + 3]) {
+      increases += 1;
+    }
+  })
+  return increases;
+}
