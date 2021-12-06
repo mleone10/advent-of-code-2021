747255a85e2d89ffae167064cf0d098eb53b628f
Mario Leone <mleone10@gmail.com>
2021-12-05 23:10:47 -0500

    Day 4.2

--- src/day04/main.ts
+++ src/day04/main.ts
@@ -24,11 +24,11 @@ rl.on("close", () => {
     return resultCards
   }, [] as number[][][])
 
-  console.log("Part 1 winning score:", playBingo(nums, cards))
-  console.log("Part 2 winning score:", letSquidWin(nums, cards))
+  console.log("Winning score:", winBingo(nums, cards))
+  console.log("Losing score:", loseBingo(nums, cards))
 })
 
-function playBingo(nums: number[], cards: number[][][]): number {
+function winBingo(nums: number[], cards: number[][][]): number {
   const called: number[] = []
 
   // There may be some way to clean this up.  Reduce nums?
@@ -46,10 +46,6 @@ function playBingo(nums: number[], cards: number[][][]): number {
   return 0
 }
 
-function letSquidWin(nums: number[], cards: number[][][]): number {
-  return 0
-}
-
 function hasWon(nums: number[], card: number[][]): boolean {
   const allCalled = (nums: number[], row: number[]) => row.every(i => nums.includes(i))
   const transposed = (card: number[][]) => card.map((_, i) => card.map(row => row[i]));
@@ -63,4 +59,26 @@ function calcScore(called: number[], card: number[][]): number {
   // Given a card, flatten the 2D array into a 1D array, remove any element that has been called.
   // Then sum them up before multiplying by the last called number.
   return card.flat().filter(i => !called.includes(i)).reduce((sum, current) => sum += current) * called[called.length - 1]
+}
+
+// Can probably refactore this to be recursive.
+function loseBingo(nums: number[], cards: number[][][]): number {
+  // Winners returns the list of winning bingo cards given a list of called numbers and a list of cards
+  const winners = (nums: number[], cards: number[][][]) => cards.filter(c => hasWon(nums, c))
+
+  // Starting with the full list of called numbers, remove them one by one.
+  for (let i = nums.length; i >= 0; i--) {
+    // For any given list of called numbers, identify those bingo cards that will no longer be winners in the next iteration.
+    // The puzzle inputs are formulated such that there will be at most one winner per called number. 
+    const newWinners = winners(nums, cards).filter(w => !winners(nums.slice(0, -1), cards).includes(w))
+
+    // When we find the first card to fall off the list of winners (i.e. not win), we've found the last winner.
+    // Calculate its score and return.
+    if (newWinners.length === 1) {
+      return calcScore(nums, newWinners[0])
+    }
+    nums.pop()
+  }
+
+  return 0
 }
