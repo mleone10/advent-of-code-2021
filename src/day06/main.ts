901f8ba955695d437feca507907f3aec9e006b41
mleone10 <mleone10@gmail.com>
2021-12-06 19:38:17 -0500

    Day 6.2 - refactor main calculation to account for memory constraints

--- src/day06/main.ts
+++ src/day06/main.ts
@@ -8,19 +8,35 @@ processStdin(ls => {
 })
 
 function calcNumFishAfterN(fish: number[], days: number) {
+  // Rather than track an array of each fish, track the number of fish by days left.
+  // E.g. instead of [3, 4, 3, 1, 2], store [0, 1, 1, 2, 1].
+  let fishBySpawnTime: number[] = []
+  fish.forEach(f => {
+    fishBySpawnTime[f] = fishBySpawnTime[f] == undefined ? 1 : fishBySpawnTime[f] += 1
+  })
+
+  // Simulate the given number of days.
   for (let i = 0; i < days; i++) {
-    const newFish: number[] = []
-
-    fish.forEach(f => {
-      if (f == 0) {
-        newFish.push(6, 8)
-      } else {
-        newFish.push(f - 1)
-      }
-    })
-
-    fish = newFish
+    fishBySpawnTime = calcNextTick(fishBySpawnTime)
   }
 
-  return fish.length
+  // The sum values in the final array represent the total number of fish.
+  return fishBySpawnTime.reduce((res, num) => {
+    return res + num
+  })
+}
+
+function calcNextTick(fishBySpawnTime: number[]): number[] {
+  const newFishBySpawnTime: number[] = []
+  fishBySpawnTime.forEach((v, i) => {
+    if (i == 0) {
+      newFishBySpawnTime[8] = v // New fish being born!
+      newFishBySpawnTime[6] = newFishBySpawnTime[6] == undefined ? v : newFishBySpawnTime[6] + v // Existing fish get reset to 6 days.
+    } else {
+      // Otherwise, just "decrement" the days left for these v fish.
+      newFishBySpawnTime[i - 1] = newFishBySpawnTime[i - 1] == undefined ? v : newFishBySpawnTime[i - 1] + v
+    }
+  })
+
+  return newFishBySpawnTime
 }
