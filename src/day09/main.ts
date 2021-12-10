fbde7a680f785edf1e639607900825a1e2461a7f
mleone10 <mleone10@gmail.com>
2021-12-10 14:40:25 -0500

    Day 8.1 - extract low point identification to a filter

--- src/day09/main.ts
+++ src/day09/main.ts
@@ -1,21 +1,27 @@
 import processStdin from "../readStdin";
 
 processStdin(ls => {
+  // Convert the list of lines to a 2D array of integers for simplicity.
+  const numGrid = ls.map(l => l.split("").map(p => parseInt(p)))
+
+  const isLowPoint = (x: number, y: number) => {
+    const neighbors = [
+      x > 0 ? numGrid[y][x - 1] : Infinity,
+      x < numGrid[0].length - 1 ? numGrid[y][x + 1] : Infinity,
+      y > 0 ? numGrid[y - 1][x] : Infinity,
+      y < numGrid.length - 1 ? numGrid[y + 1][x] : Infinity
+    ]
+
+    return neighbors.every(n => n > numGrid[y][x])
+  }
+
+  // For Part 1, iterate through each point and check if it's the smallest of it's cardinal neighbors
   console.log(
     "Part 1:",
-    ls
-      .reduce((sum, line, iLine, grid) => {
-        const numGrid = grid.map(l => l.split("").map(p => parseInt(p)))
-        return sum + numGrid[iLine].reduce((sum, point, iPoint, numLine) => {
-          const isLow = [-1, 0, 1].reduce((isLow, dY) => {
-            return isLow && [-1, 0, 1].reduce((isLow, dX) => {
-              const x = iPoint + dX
-              const y = iLine + dY
-              return x >= 0 && y >= 0 && x < numLine.length && y < numGrid.length ? isLow && numGrid[y][x] >= point : isLow
-            }, true)
-          }, true)
-          return sum + (isLow ? 1 + point : 0)
-        }, 0)
+    numGrid.reduce((sum, line, iLine) => {
+      return sum + line.filter((_, i) => isLowPoint(i, iLine)).reduce((sum, p) => {
+        return sum + p + 1
       }, 0)
+    }, 0)
   )
 })
