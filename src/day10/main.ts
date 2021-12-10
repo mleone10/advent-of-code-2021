24e38913009172e8e21136ec3567ea49f2cf7704
mleone10 <mleone10@gmail.com>
2021-12-10 16:56:46 -0500

    Day 10.1

---
+++ src/day10/main.ts
@@ -0,0 +1,29 @@
+import processStdin from "../readStdin";
+
+const charPairs = new Map([["(", ")"], ["[", "]"], ["{", "}"], ["<", ">"]])
+const charScores = new Map([[")", 3], ["]", 57], ["}", 1197], [">", 25137]])
+
+processStdin(ls => {
+  // For Part 1, identify the first incorrect character in each line, remove blanks (non-currupt lines), and add up the scores.
+  console.log(
+    "Part 1",
+    ls.map(l => findFirstIncorrectChar(l, [])).filter(c => c).map(c => charScores.get(c) || 0).reduce((sum, s) => sum + s)
+  )
+})
+
+function findFirstIncorrectChar(l: string, cs: string[]): string {
+  if (l.length == 0) {
+    return ""
+  }
+
+  if (Array.from(charPairs.keys()).includes(l[0])) {
+    cs.push(l[0])
+  } else {
+    const c = cs.pop() || ""
+    if (charPairs.get(c) != l[0]) {
+      return l[0]
+    }
+  }
+
+  return findFirstIncorrectChar(l.slice(1), cs)
+}
