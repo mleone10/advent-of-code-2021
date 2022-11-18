5a23238ca8d38f590602843853c6f5089896acea
Mario Leone <mleone10@gmail.com>
2022-11-18 15:19:55 -0500

    Save progress

--- src/day12/day12.go
+++ src/day12/day12.go
@@ -1,25 +1,25 @@
 package day12
 
 import (
+	"log"
 	"strings"
 )
 
 type caveLabel string
+type path []caveLabel
 
 type Day12 struct {
 	Input  string
 	system map[caveLabel][]caveLabel
+	paths  [][]caveLabel
 }
 
 func (d Day12) SolvePartOne() (int, error) {
 	d.init()
 
-	paths, err := d.countPaths()
-	if err != nil {
-		return 0, err
-	}
+	d.computePaths(path{}, caveLabel("start"))
 
-	return paths, nil
+	return 0, nil
 }
 
 func (d Day12) SolvePartTwo() (int, error) {
@@ -48,6 +48,30 @@ func (d *Day12) init() {
 	d.system = system
 }
 
-func (d Day12) countPaths() (int, error) {
-	return 0, nil
+func (d Day12) computePaths(path path, cur caveLabel) {
+	log.Printf("hello?")
+	if cur == "end" {
+		path = append(path, "end")
+		log.Printf("terminal path: %v", path)
+		d.paths = append(d.paths, path)
+		return
+	}
+
+	for _, neighbor := range d.system[cur] {
+		if neighbor.isSmallCave() && path.contains(neighbor) {
+			continue
+		}
+		path = append(path, neighbor)
+		log.Printf("continuing path: %v", path)
+		d.computePaths(path, neighbor)
+	}
+}
+
+func (c caveLabel) isSmallCave() bool {
+	cave := string(c)
+	return strings.ToLower(cave) == string(cave)
+}
+
+func (p path) contains(c caveLabel) bool {
+	return false
 }
