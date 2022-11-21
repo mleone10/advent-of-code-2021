9d5ead6624413adca44ad7f4811b77fbd91f8edd
Mario Leone <mleone10@gmail.com>
2022-11-20 20:28:43 -0500

    Finish day 13

--- src/day13/day13.go
+++ src/day13/day13.go
@@ -1,26 +1,24 @@
 package day13
 
 import (
+	"io"
+	"log"
 	"strconv"
 	"strings"
 )
 
 type Puzzle struct {
-	Paper []dot
+	Paper map[int]map[int]bool
 	Insts []instruction
 }
 
-type dot struct {
-	x, y int
-}
-
 type instruction struct {
-	coord int
-	left  bool
+	coord    int
+	leftFold bool
 }
 
 func NewPuzzle(input string) (Puzzle, error) {
-	paper := []dot{}
+	paper := map[int]map[int]bool{}
 	insts := []instruction{}
 
 	for _, l := range strings.Split(input, "\n") {
@@ -33,16 +31,20 @@ func NewPuzzle(input string) (Puzzle, error) {
 			if err != nil {
 				return Puzzle{}, err
 			}
-			paper = append(paper, dot{x, y})
+			if paper[y] == nil {
+				paper[y] = map[int]bool{}
+			}
+			paper[y][x] = true
 		} else if axis, coordStr, ok := strings.Cut(l, "="); ok {
 			coord, err := strconv.Atoi(coordStr)
 			if err != nil {
 				return Puzzle{}, err
 			}
-			if axis == "y" {
-				insts = append(insts, instruction{coord, true})
-			} else {
+			if axis[len(axis)-1] == 'y' {
+				log.Println()
 				insts = append(insts, instruction{coord, false})
+			} else {
+				insts = append(insts, instruction{coord, true})
 			}
 		}
 	}
@@ -51,4 +53,69 @@ func NewPuzzle(input string) (Puzzle, error) {
 		Paper: paper,
 		Insts: insts,
 	}, nil
+}
+
+func (p *Puzzle) Step() {
+	inst := p.Insts[0]
+	p.Insts = p.Insts[1:]
+
+	if inst.leftFold {
+		p.foldLeft(inst.coord)
+	} else {
+		p.foldUp(inst.coord)
+	}
+}
+
+func (p *Puzzle) foldLeft(x int) {
+	for i, row := range p.Paper {
+		for j := range row {
+			if j < x {
+				continue
+			}
+			newX := x - (j - x)
+			p.Paper[i][newX] = true
+			delete(p.Paper[i], j)
+		}
+	}
+}
+
+func (p *Puzzle) foldUp(y int) {
+	for i, row := range p.Paper {
+		if i < y {
+			continue
+		}
+		for j := range row {
+			newY := y - (i - y)
+			if p.Paper[newY] == nil {
+				p.Paper[newY] = map[int]bool{}
+			}
+			p.Paper[newY][j] = true
+			delete(p.Paper[i], j)
+		}
+	}
+}
+
+func (p Puzzle) NumDots() int {
+	num := 0
+	for _, row := range p.Paper {
+		for range row {
+			num += 1
+		}
+	}
+	return num
+}
+
+func (p Puzzle) Output(w io.Writer) {
+	for i := 0; i < len(p.Paper); i++ {
+		line := []byte{}
+		for j := 0; j < 80; j++ {
+			if _, ok := p.Paper[i][j]; ok {
+				line = append(line, byte('#'))
+			} else {
+				line = append(line, byte('.'))
+			}
+		}
+		line = append(line, byte('\n'))
+		w.Write(line)
+	}
 }
